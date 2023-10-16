6c18df131da43ce2f9bb2a1d7d16642c484abfc0
Umut <uggeneralmaterial@gmail.com>
2023-10-16 23:27:58 +0300

    Add files via upload

---
+++ yaml/udemy/Intro_to_YAML_A_handsoncourse_1saat/sachin_read_yaml.py
@@ -0,0 +1,10 @@
+import yaml
+
+yaml_file = 'sachin.yaml'
+
+with open(yaml_file, encoding="utf-8") as file:
+    data = yaml.load(file, loader=yaml.FullLoader)
+
+    for keys, values in data.items():
+        #key:keyname, value
+        print("key: ", keys, "\t value:", values, "\t type:", values)
